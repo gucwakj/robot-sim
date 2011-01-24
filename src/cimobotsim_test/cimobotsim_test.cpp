87218c6ebd80bd848105dfa825c381d434961c08
Kevin Gucwa <gucwakj@gmail.com>
2011-01-24 12:08:45 -0800

    build array to replicate video

--- src/cimobotsim_test/cimobotsim_test.cpp
+++ src/cimobotsim_test/cimobotsim_test.cpp
@@ -10,41 +10,34 @@ dReal vel[] = {	1,	1,	1,	1,
 		1,	1,	1,	1,
 		1,	1,	1,	1};
 */
-/* move_forward() */
-/*dReal ang[] = {	0,	-60,	0,	0,
-		0,	-60,	-60,	0,
-		0,	0,	-60,	0,
-		0,	0,	0,	0};
-dReal vel[] = {	1,	1,	1,	1,
-		1,	1,	1,	1,
-		1,	1,	1,	1,
-		1,	1,	1,	1};
-*/
-/* move_right() */
-/*dReal ang[] = {	360,	0,	0,	-360};
-dReal vel[] = {	1,	1,	1,	1};
-*/
-/* stand() */
-dReal ang[] = {	0,	0,	-95.5293,	0,
-		0,	0,	-95.5293,	45,
-		0,	-85,	123456789,	0,
-		0,	-90,	10,		0,
-		0,	-90,	10,		45};
-dReal vel[] = {	1,	1,	1,	1,
-		1,	1,	1,	1,
-		1,	1,	1,	1,
-		1,	1,	0.1,	1,
-		1,	1,	1,	1};
+/* video replication */
+dReal ang[] = {	0,	-60,		0,		0, // forward
+				0,	-60,	-60,	0,
+				0,	0,		-60,	0,
+				0,	0,	0,	0,
+				720,	0,	0,	-720,		// right
+				0,	0,	-95.5293,	0,		// camera stand
+				0,	0,	-95.5293,	45,
+				0,	-85,	123456789,	0,
+				0,	-90,	10,		0,
+				0,	-90,	10,		45};
+dReal vel[] = {	1,	1,	1,	1,		// forward
+				1,	1,	1,	1,
+				1,	1,	1,	1,
+				1,	1,	1,	1,
+				1,	1,	1,	1,		// right
+				1,	1,	1,	1,		// camera stand
+				1,	1,	1,	1,
+				1,	1,	1,	1,
+				1,	1,	0.1,	1,
+				1,	1,	1,	1};
 
 int main(int argc, char* argv[]) {
 	/*CiMobotSim *sim = new CiMobotSim(1, 4, 1, 5.0, 0.3, 0.1, 0.45, 0.45, ang, vel);*/ /*test*/
-	/*CiMobotSim *sim = new CiMobotSim(1, 4, 1, 5.0, 0.3, 0.1, 0.45, 0.45, ang, vel);*/ /*move_forward*/
-	/*CiMobotSim *sim = new CiMobotSim(1, 1, 1, 5.0, 0.3, 0.1, 0.45, 0.45, ang, vel);*/ /*move_right*/
-	CiMobotSim *sim = new CiMobotSim(1, 5, 1, 5.0, 0.3, 0.1, 0.45, 0.45, ang, vel); /*stand*/
+	CiMobotSim *sim = new CiMobotSim(1,10, 1, 10.0, 0.3, 0.1, 0.45, 0.45, ang,vel);		/*video*/
 
 	sim->groundPlane(0, 0, 0, 1, 0);
 	sim->iMobotBuild(0, 0, 0, 0);
-	//sim->printData();
 	sim->run(argc, argv);
 	sim->replyMessage();
 
