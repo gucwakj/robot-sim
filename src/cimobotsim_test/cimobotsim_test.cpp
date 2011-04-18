baf21b174159249ea5e6ce9c7096bdfdbccccf0f
Kevin Gucwa <gucwakj@gmail.com>
2011-04-17 19:31:53 -0700

    created a few videos for single module

--- src/cimobotsim_test/cimobotsim_test.cpp
+++ src/cimobotsim_test/cimobotsim_test.cpp
@@ -11,11 +11,11 @@ dReal vel[] = {	1,	1,	1,	1,
 		1,	1,	1,	1};
 */
 /* video replication */
-/*dReal ang[] = {	0,	-60,		0,		0, // forward
-				0,	-60,	-60,	0,
-				0,	0,		-60,	0,
+/*dReal ang[] = {	0,	-30,		0,		0, // forward
+				0,	-30,	-30,	0,
+				0,	0,		-30,	0,
 				0,	0,	0,	0,
-				720,	0,	0,	-720,		// right
+				360,	0,	0,	-360,		// right
 				0,	0,	-95.5293,	0,		// camera stand
 				0,	0,	-95.5293,	45,
 				0,	-85,	123456789,	0,
@@ -32,19 +32,18 @@ dReal vel[] = {	1,	1,	1,	1,		// forward
 				1,	1,	0.1,	1,
 				1,	1,	1,	1};
 */
-
 /* inchworm forward */
-/*dReal ang[] = {	0, -60, 0, 0,
-				0, -60, -60, 0,
-				0, 0, -60, 0,
+dReal ang[] = {	0, -30, 0, 0,
+				0, -30, -30, 0,
+				0, 0, -30, 0,
 				0, 0, 0, 0,
-				0, -60, 0, 0,
-				0, -60, -60, 0,
-				0, 0, -60, 0,
+				0, -30, 0, 0,
+				0, -30, -30, 0,
+				0, 0, -30, 0,
 				0, 0, 0, 0,
-				0, -60, 0, 0,
-				0, -60, -60, 0,
-				0, 0, -60, 0,
+				0, -30, 0, 0,
+				0, -30, -30, 0,
+				0, 0, -30, 0,
 				0, 0, 0, 0};
 dReal vel[] = {	1,	1,	1,	1,
 				1,	1,	1,	1,
@@ -58,17 +57,17 @@ dReal vel[] = {	1,	1,	1,	1,
 				1,	1,	1,	1,
 				1,	1,	1,	1,
 				1,	1,	1,	1};
-*/
-/* rolling */
+
+
+/* // rolling
 dReal ang[] = {270, 0, 0, -270};
-dReal vel[] = {1, 1, 1, 1};
+dReal vel[] = {1, 1, 1, 1};*/
 
 int main(int argc, char* argv[]) {
 	/*CiMobotSim *sim = new CiMobotSim(1, 4, 1, 5.0, 0.3, 0.1, 0.45, 0.45, ang, vel);*/ /*test*/
-	/*CiMobotSim *sim = new CiMobotSim(1,10, 1, 10.0, 0.3, 0.1, 0.45, 0.45,ang,vel);*/		/*video*/
-	/*CiMobotSim *sim = new CiMobotSim(1,12, 1, 12.0, 0.2, 0.1, 0.45,
-0.45,ang,vel); //inchworm */
-	CiMobotSim *sim = new CiMobotSim(1,1, 1, 12.0, 0.4, 0.1, 0.45,0.45,ang,vel);
+	//CiMobotSim *sim = new CiMobotSim(1,10, 1, 15.0, 0.3, 0.1, 0.45, 0.45,ang,vel);		/*video*/
+	CiMobotSim *sim = new CiMobotSim(1,12, 1, 12.0, 0.8, 0.1, 0.45, 0.45, ang, vel); //inchworm
+	//CiMobotSim *sim = new CiMobotSim(1,1, 1, 12.0, 0.4, 0.1, 0.45,0.45,ang,vel);
 
 	sim->groundPlane(0, 0, 0, 1, 0);
 	sim->iMobotBuild(0, 0, 0, 0);
