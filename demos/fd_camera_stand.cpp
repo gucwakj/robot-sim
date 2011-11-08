12f51bad06d50a53525bc6a7529217d3369e620a
Kevin Gucwa <gucwakj@gmail.com>
2011-11-08 15:00:10 -0800

    changed demos programs to compile
    linking error to a texture from ODE so code will
    not run properly

--- demos/fd_camera_stand.cpp
+++ demos/fd_camera_stand.cpp
@@ -3,12 +3,9 @@
 dReal ang[] = {	0,	0,	90,	0,
 				0,	85, 123456789, 0,
 				0,	85, 0, 0};
-dReal vel[] = {1};
-vel[10] = 0.1;
 
 int main(int argc, char* argv[]) {
 	CiMobotSim *sim = new CiMobotSim(1, 3, 1, ang);
-	sim->setAngVel(vel);
 
 	sim->groundPlane(0, 0, 0, 1, 0);
 	sim->iMobotBuild(0, 0, 0, 0);
