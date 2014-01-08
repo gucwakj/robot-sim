dc4984b192e4a72b8c51563fdbce6a79634a01dc
Kevin Gucwa <gucwakj@gmail.com>
2014-01-08 13:29:59 -0800

    changed test to turn code to test timing variability

--- demos/linkbot/test.cpp
+++ demos/linkbot/test.cpp
@@ -6,10 +6,12 @@ int main(int argc, char *argv[]) {
 	CLinkbotI robot1;
 
 	robot1.connect();
-	robot1.movexyTo(1, 1, 1.75, 3.69);
-	robot1.movexy(0, 1, 1.75, 3.69);
-	robot1.setColorRGB(255, 0, 0);
-	robot1.movexyTo(0, 1, 1.75, 3.69);
+
+	robot1.moveForward(180);
+	robot1.turnLeft(90, 1.75, 3.69);
+	robot1.moveForward(180);
+	robot1.turnRight(90, 1.75, 3.69);
+	robot1.moveForward(180);
 
 	return 0;
 }
