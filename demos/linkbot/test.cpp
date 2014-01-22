ad344d0017cb7cfe1aca3496d4f3bd7bebbe2cd8
Kevin Gucwa <gucwakj@gmail.com>
2014-01-22 09:33:47 -0800

    fixes in ch source to make line and text work

--- demos/linkbot/test.cpp
+++ demos/linkbot/test.cpp
@@ -7,11 +7,8 @@ int main(int argc, char *argv[]) {
 
 	robot1.connect();
 	robot1.setTwoWheelRobotSpeed(3, 1.75);
-printf("forward\n");
 	robot1.moveForward(360);
-printf("backward\n");
 	robot1.moveBackward(360);
-printf("distance\n");
 	robot1.moveDistance(5, 1.75);
 
 	return 0;
